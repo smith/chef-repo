881eaad43b51d0d5cfa61d5eedc6e1de0227c6a6
Nathan L Smith <nlloyds@gmail.com>
2011-04-17 21:35:38 -0500

    demo cookbook

---
+++ cookbooks/demo/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Nathan L Smith"
+maintainer_email "nlloyds@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures demo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
