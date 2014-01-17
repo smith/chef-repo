603c9db48899b38ce9bde27fc401f357b5c25581
Nathan L Smith <nlloyds@gmail.com>
2014-01-17 00:11:32 -0600

    berks & cookbook cleanup

--- Berksfile
+++ Berksfile
@@ -1,11 +1,3 @@
 site :opscode
 
-cookbook 'apache2'
-cookbook 'mysql'
-cookbook 'newrelic'
-cookbook 'php'
-cookbook 'postfix'
-cookbook 'sudo'
-cookbook 'vim'
-
 cookbook 'nlsmith', path: './cookbooks/nlsmith'
--- cookbooks/nlsmith/attributes/default.rb
+++ cookbooks/nlsmith/attributes/default.rb
@@ -8,18 +8,21 @@
 #
 
 # Tone down apache settings for the tiny server
-override['apache']['contact'] = 'smith@nlsmith.com'
-override['apache']['prefork']['startservers']        = 1
-override['apache']['prefork']['minspareservers']     = 1
-override['apache']['prefork']['maxspareservers']     = 5
-override['apache']['prefork']['serverlimit']         = 50
-override['apache']['prefork']['maxclients']          = 50
-override['apache']['prefork']['maxrequestsperchild'] = 5000
+set['apache']['contact'] = 'smith@nlsmith.com'
+set['apache']['prefork']['startservers']        = 1
+set['apache']['prefork']['minspareservers']     = 1
+set['apache']['prefork']['maxspareservers']     = 5
+set['apache']['prefork']['serverlimit']         = 50
+set['apache']['prefork']['maxclients']          = 50
+set['apache']['prefork']['maxrequestsperchild'] = 5000
 
 # And for mysql
-override['mysql']['bind_address']                       = '127.0.0.1'
-override['mysql']['tunable']['innodb_buffer_pool_size'] = '64M'
+set['mysql']['bind_address']                       = '127.0.0.1'
+set['mysql']['tunable']['innodb_buffer_pool_size'] = '64M'
+
+# Newrelic
+set['newrelic']['startup_mode'] = 'external'
 
 # Sudo options
-override['authorization']['sudo']['groups']       = %w[ admin ]
-override['authorization']['sudo']['passwordless'] = true
+set['authorization']['sudo']['groups']       = %w[ admin ]
+set['authorization']['sudo']['passwordless'] = true
