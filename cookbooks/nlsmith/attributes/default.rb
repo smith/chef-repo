9cfd40e4959095b5df2600ac1f8a09f74e3aead4
Nathan L Smith <smith@chef.io>
2015-05-14 08:39:06 -0500

    turn down apache settings

--- cookbooks/nlsmith/attributes/default.rb
+++ cookbooks/nlsmith/attributes/default.rb
@@ -12,9 +12,9 @@ set['apache']['contact'] = 'smith@nlsmith.com'
 set['apache']['prefork']['startservers']        = 1
 set['apache']['prefork']['minspareservers']     = 1
 set['apache']['prefork']['maxspareservers']     = 5
-set['apache']['prefork']['serverlimit']         = 50
-set['apache']['prefork']['maxclients']          = 50
-set['apache']['prefork']['maxrequestsperchild'] = 5000
+set['apache']['prefork']['serverlimit']         = 5
+set['apache']['prefork']['maxclients']          = 5
+set['apache']['prefork']['maxrequestsperchild'] = 300
 
 # And for mysql
 set['mysql']['bind_address']                       = '127.0.0.1'
